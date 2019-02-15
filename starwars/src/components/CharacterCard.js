278b247718405ffe74bbb11fd646b39f368c4d1d
Dan Sherman <dvsherman@protonmail.com>
2019-02-15 10:03:43 -0700

    created CharacterCard component, added name and birth year elements, moving on to styling

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,2 +1,13 @@
 import React from 'react';
+import './StarWars.css';
 
+const CharacterCard = props => {
+    return (
+        <div className="character-card">
+            <h3>{props.character.name}</h3>
+            <p><strong>Born:</strong> {props.character.birth_year}</p>
+        </div>
+    )
+}
+
+export default CharacterCard;
